6034ad5c730d1fdc45fa2e0f4071577591b97985
m3ia <meianatividad@gmail.com>
2022-09-06 14:30:44 -0700

    move react app up one dir

--- server/index.js
+++ server/index.js
@@ -7,6 +7,34 @@ const PORT = 8080;
 
 // Cors middleware
 app.use(cors());
+
 app.listen(PORT, () => {
   console.log(`Hola this server is running on port ${PORT}`);
+});
+
+// Tells server what directory we're working on
+const __dirname = path.resolve();
+
+// Render static files from client folder
+app.use(express.static('../client'));
+
+// First fetch request
+app.get('/weather', (req, res) => {
+  const city = req.query.cityName;
+  const apiKey = process.env.API_KEY;
+  const params = new URLSearchParams({
+    q: req.query.cityName,
+    appid: process.env.API_KEY,
+    units: 'imperial',
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
