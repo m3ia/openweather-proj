11486a788a41dde301a95a4f69dfad97953a536b
m3ia <meianatividad@gmail.com>
2022-09-06 21:21:59 -0700

    add .env

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 8080;
+
+dotenv.config();
 
 // Cors middleware
 app.use(cors());
@@ -11,12 +14,6 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Hola this server is running on port ${PORT}`);
 });
-
-// Tells server what directory we're working on
-const __dirname = path.resolve();
-
-// Render static files from client folder
-app.use(express.static('../client'));
 
 app.get('/', (req, res) => {
   res.json('Hello from Techtonica');
@@ -34,13 +31,14 @@ app.get('/api/students', (req, res) => {
   res.json(STUDENTS);
 })
 
+const API_KEY = process.env.OPENWEATHER_API_KEY;
 // First fetch request
 app.get('/weather', (req, res) => {
   const city = req.query.cityName;
-  const apiKey = process.env.API_KEY;
+  const apiKey = API_KEY;
   const params = new URLSearchParams({
     q: req.query.cityName,
-    appid: process.env.API_KEY,
+    appid: apiKey,
     units: 'imperial',
   });
   const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
@@ -54,3 +52,4 @@ app.get('/weather', (req, res) => {
       console.log(err);
     });
 });
+
